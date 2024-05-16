3bbac880608593c74bdb7fbe781c3fb5b66464cb
Henry Ocampo <henryocampo@Henrys-MacBook-Pro.local>
2024-05-15 19:58:23 -0600

    ading last changes

---
+++ best/rh/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import StaffAction from './components/staffAction/';
+
+function App() {
+  return (
+    <div className="App">
+      <StaffAction />
+    </div>
+  );
+}
+
+export default App;
